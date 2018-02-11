0ce48fb1797b17789e7fbab7687b8f7b12e46145
patrickatcw <pj8392@yahoo.com>
2018-02-11 11:24:48 -0500

    redo challenge

--- src/Challenge.java
+++ src/Challenge.java
@@ -18,39 +18,12 @@ public class Challenge {
 
     public static void main(String[] args) {
 
-        int highScorePosition = calculateHighScorePosition(1500);  //calling function and giving it to int value
-        displayHighScorePosition("Tim", highScorePosition);
 
-        highScorePosition = calculateHighScorePosition(900);        //redefining value
-        displayHighScorePosition("Bob", highScorePosition);
-
-        highScorePosition = calculateHighScorePosition(400);
-        displayHighScorePosition("Percy", highScorePosition);
-
-        highScorePosition = calculateHighScorePosition(50);
-        displayHighScorePosition("Gilbert", highScorePosition);
 
     }
 
-    public static void displayHighScorePosition(String playerName, int highScorePosition) {
 
-        System.out.println(playerName + " managed to get into position and they got a " + highScorePosition +
-                " on the high score table");
 
-    }
-
-    public static int calculateHighScorePosition(int playerScore) {
-
-        if (playerScore >= 1000) {
-            return 1;
-        } else if (playerScore >= 500 && playerScore < 1000) {
-            return 2;
-        } else if (playerScore >= 100 && playerScore < 500) {
-            return 3;
-        } else {
-            return 4;
-        }
-    }
 }
 
 
